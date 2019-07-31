0d27b822310c09eddedd4957d27ffc7f0fba6519
Michael Dugah <michael.dugah@softwaregroup.com>
2019-07-31 10:58:48 +0000

    transpose md5utils from java to kotlin

--- app/src/main/java/com/personal/tools/checksumkt/MD5Utils.kt
+++ app/src/main/java/com/personal/tools/checksumkt/MD5Utils.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
+import java.math.BigInteger
 import java.security.MessageDigest
 
 class MD5Utils {
@@ -17,5 +18,23 @@ class MD5Utils {
     fun calculateDigest(file: File?): String {
         val digest: MessageDigest = MessageDigest.getInstance("MD5")
         val inputStream: InputStream = FileInputStream(file)
+
+        var buffer = ByteArray(8192)
+        var read: Int
+        try {
+            while (inputStream.read() > 0) {
+                read = inputStream.read(buffer)
+                digest.update(buffer, 0, read);
+            }
+        }finally {
+            inputStream.close()
+        }
+
+        val md5sum: ByteArray = digest.digest();
+        val bigInt: BigInteger = BigInteger(1, md5sum)
+        var output: String = bigInt.toString(16)
+
+        output = String.format("%32", output).replace(' ', '0')
+        return output
     }
 }
--- app/src/main/java/com/personal/tools/checksumkt/MainActivity.kt
+++ app/src/main/java/com/personal/tools/checksumkt/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.personal.tools.checksumkt
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     var downloadUrl: String = "";
-    val downloadedFile: File? = null
+    var downloadedFile: File? = null
     var downloadLinkText: EditText? = null;
     var resultText: EditText? = null;
 
@@ -22,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button)
         resultText = findViewById(R.id.checksumResult)
 
-        downloadLinkText?.setText("1S-z5JJkEs6sJxUgA5rtXsHEnK9h4W4XW")
+        downloadLinkText?.setText("1FUmsSGn_KjeQD14_ajctKFGzJef4nLBl")
         downloadUrl =
             "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=" + downloadLinkText?.getText().toString()
 
@@ -45,6 +53,10 @@ class MainActivity : AppCompatActivity() {
                 return md5Checker.getMD5(downloadedFile)
             }
 
+            override fun onPostExecute(result: String?) {
+                resultText?.setText(result)
+            }
+
         }
 
         Task().execute()
@@ -52,9 +64,25 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun downloadApk(url: String) {
+    fun downloadApk(urlString: String) {
+        val url = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        try {
+            connection.connect()
 
+            val cw = ContextWrapper(this)
+            val directory = cw.getDir("media", Context.MODE_PRIVATE)
+            val pathToFile = String.format("%s/%s.apk", directory, "checksumfile")
+            downloadedFile = File(pathToFile)
+            val fos = FileOutputStream(downloadedFile)
+            copyStream(connection.inputStream, fos)
+        } finally {
+            connection.disconnect()
+        }
     }
 
+    fun copyStream(inputStream: InputStream, outputStream: OutputStream){
+
+    }
 
 }
